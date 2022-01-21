d390e1c05e8966fd766469a8dc49e99fd8475e0d
marcosqlbi <marco@sqlbi.com>
2022-01-21 12:12:50 +0100

    Updated todo

--- TestDaxTemplates/Program.cs
+++ TestDaxTemplates/Program.cs
@@ -15,3 +15,37 @@ namespace TestDaxTemplates
         }
     }
 }
+
+/*
+TODO
+
+add description to config file
+copy format original measure
+combine different time intelligence into the same date table
+wildcard for measure selection
+override table for measures of template
+DISPLAYFOLDER of original measure
+Cascading match for IsoTranslation (support region only for translations files?)
+Add flag to expose parameters to Bravo UI
+
+CHeck DISPLAYFOLDERRULE for fixed measure (e.g. _ShowValueForDates)
+Include multiple measure template files (calendar+fiscal) with different DisplayFolderRule
+API: 
+- List of available templates
+    - Include config for each model
+    - Localization of description - it should be a reference to UI names
+
+- Preview of upcoming changes
+    - Try to apply the model
+    - Do not save changes
+    - Extract modified entities (added tables, added measures)
+    - Preview of added tables (first 50, visible/hidden flag, using the translated name for reference table)
+    - List of measures from template with full path + DAX
+ 
+
+Fiscal Year is wrong on template 01
+
+Implement format with all-in-one-file (Config:{}, TimeIntelligence-01:{}}...
+
+
+ * */
