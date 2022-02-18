a58195ac946f82b8c7cca7c8ecb4ad4c7bb79c07
Alberto Spelta <alberto.spelta@live.com>
2022-02-18 14:46:12 +0100

    Fix exception message

--- Dax.Template/Model/ModelChanges.cs
+++ Dax.Template/Model/ModelChanges.cs
@@ -235,7 +235,7 @@ namespace Dax.Template.Model
             queryTablesDefinition ??= string.Empty;
 
             string daxQuery = $"{queryTablesDefinition}\r\nEVALUATE TOPNSKIP ( {previewRows}, 0, {tableExpression} )";
-            System.IO.File.WriteAllText(@"c:\temp\query.dax", daxQuery);
+            System.Diagnostics.Debug.WriteLine(daxQuery);
             if (connection.State != System.Data.ConnectionState.Open) connection.Open();
             using AdomdCommand command = new(daxQuery, connection);
             using var reader = command.ExecuteReader();
